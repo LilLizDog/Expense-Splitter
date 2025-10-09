4731546bb5762e9818c98bcc053ec08f492cd2f8
Preet Kaur <preet.kaur@slu.edu>
2025-10-09 11:00:33 -0500

    Updated main.py with HTML routes and requirements

--- app/main.py
+++ app/main.py
@@ -2,12 +2,21 @@
 # This is the main entry point for our FastAPI app.
 # It connects everything — routes, Supabase setup, and basic health checks.
 
-from fastapi import FastAPI                 # main FastAPI class to make our app
+from fastapi import FastAPI, Request          # main FastAPI class + request object
+from fastapi.responses import HTMLResponse  # used to return HTML pages
+from fastapi.templating import Jinja2Templates  # for rendering HTML templates
+# from fastapi.staticfiles import StaticFiles  # to serve static files like CSS/JS  (might not need this)
 from .core.supabase_client import supabase  # connects our app to Supabase (database)
 from .routers import groups, expenses,balances, auth       # brings in the /groups, /expenses and /balances route files we made
 
 # create the main app object
 app = FastAPI(title="Expense Splitter API")  # title just shows up on the docs page
+
+#setup teplate rendering for HTML pages (app/templates folder)
+templates = Jinja2Templates(directory="app/templates")
+
+# static folder if we add CSS or JS file later(keep in mind to uncomment the import as well)
+# app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 # ------------------------
 # BASIC TEST ROUTES
@@ -46,3 +55,35 @@ app.include_router(balances.router)   # links all /balances endpoints into our m
 
 # add the /auth routes from the auth.py file
 app.include_router(auth.router)       # links all /auth endpoints into our main app
+
+# ------------------------
+# FRONTEND HTML ROUTES
+# ------------------------
+
+# main login page
+@app.get("/", response_class=HTMLResponse)
+async def get_login(request: Request):
+    # displays login page
+    return templates.TemplateResponse("login.html", {"request": request})
+
+# alternate login routes
+@app.get("/login", response_class=HTMLResponse)
+async def get_login_alias(request: Request):
+    # serves same login page at /login
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@app.get("/login.html", response_class=HTMLResponse)
+async def get_login_html(request: Request):
+    # handles direct reference to login.html
+    return templates.TemplateResponse("login.html", {"request": request})
+
+# signup page
+@app.get("/signup", response_class=HTMLResponse)
+async def get_signup(request: Request):
+    # displays signup page
+    return templates.TemplateResponse("signup.html", {"request": request})
+
+@app.get("/signup.html", response_class=HTMLResponse)
+async def get_signup_html(request: Request):
+    # handles direct reference to signup.html
+    return templates.TemplateResponse("signup.html", {"request": request})
